91cc635c5a7be62ceed770bf81692d98948ef516
1980mmaria <artemiotantaricocueva@gmail.com>
2024-09-06 17:02:58 -0500

    Redes Sociales

---
+++ btn-redes-sociales/main.js
@@ -0,0 +1,8 @@
+let box_load = document.querySelector("box-load")
+document.addEventListener("DOMContentLoaded",()=> {
+    setTimeout(() => {
+        box.load.classList.toggle("hidden");
+    },100);
+});
+
+
